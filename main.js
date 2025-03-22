4e596b427edca0863250560c5d66d6f35f3217d2
Zahir <176171770+Zahir091@users.noreply.github.com.>
2025-03-22 19:30:23 -0400

    Added Buttons dynamically

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-main = document.querySelector(".btn-row");
+mainGroup = document.querySelector(".btn-group");
+let screenDisplay = 0;
 
 const ButtonArray = [];
+
+
 class Button {
     constructor(value) {
         this.value = value;
         this.buttonStruct = document.createElement('button');
         this.buttonStruct.classList.add('buttonStyle');
+        this.buttonStruct.id = `${value}`;
         this.buttonStruct.textContent = value;
     }
 }
 
-for(let i=1; i<10; i++) {
-    const numButton = new Button(i);
-    numButton.buttonStruct.id = `${i}`;
-    main.append(numButton.buttonStruct);
+
+
+function generateRow() {
+    let n = 1;
+    for(let j=1; j<4; j++) {
+        const btnRow = document.createElement('div');
+        btnRow.classList.add('btn-row');
+        for(let i=n; i<(n+3); i++) {
+            const btn = new Button(i);
+            btnRow.append(btn.buttonStruct);
+        }
+        n+=3;
+        mainGroup.append(btnRow);
+        console.log(mainGroup);
+    }
+
 }
 
-
+generateRow();
 
