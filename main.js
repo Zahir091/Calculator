21aab033e435c43e72947833a236a397efa75d71
Zahir <176171770+Zahir091@users.noreply.github.com.>
2025-03-22 20:00:56 -0400

    Added all buttons

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ mainGroup = document.querySelector(".btn-group");
 let screenDisplay = 0;
 
 const ButtonArray = [];
-
+const operations = ['/', 'x', '-', '+', '='];
+operations.reverse();
 
 class Button {
     constructor(value) {
@@ -15,22 +16,60 @@ class Button {
 }
 
 
-
-function generateRow() {
-    let n = 1;
-    for(let j=1; j<4; j++) {
+function genCalc() {
+    let n = 1; //nth row;
+    let number = 0; //value of numbered buttons
+    for(let j=1; j<6; j++) {
         const btnRow = document.createElement('div');
         btnRow.classList.add('btn-row');
-        for(let i=n; i<(n+3); i++) {
-            const btn = new Button(i);
-            btnRow.append(btn.buttonStruct);
+        for(let i=n; i<(n+4); i++) { //i represents nth button
+           if( i=== 1) {
+                const btn = new Button('cancel');
+                btnRow.append(btn.buttonStruct);
+           }
+           else if (i===2) {
+                const btn = new Button('+ / -');
+                btnRow.append(btn.buttonStruct);
+           }
+           else if (i === 3) {
+                const btn = new Button('%');
+                btnRow.append(btn.buttonStruct);
+           }
+            else if(i%4 != 0) {
+                number++;
+                switch(number) {
+                    case 10:  {
+                        const btn = new Button('sqrt()');
+                        btnRow.append(btn.buttonStruct);
+                        break;
+                    }
+                    case 11:  {
+                        const btn = new Button('0');
+                        btnRow.append(btn.buttonStruct);
+                        break;
+                    }
+                    case 12:  {
+                        const btn = new Button('.');
+                        btnRow.append(btn.buttonStruct);
+                        break;
+                    }
+                    default: {
+                        const btn = new Button(number);
+                        btnRow.append(btn.buttonStruct);
+                    } 
+                }
+    
+            }
+            else {
+                const btn = new Button(operations.pop());
+                btnRow.append(btn.buttonStruct);
+            }
         }
-        n+=3;
+        n += 4;
         mainGroup.append(btnRow);
         console.log(mainGroup);
     }
-
 }
 
-generateRow();
+genCalc();
 
